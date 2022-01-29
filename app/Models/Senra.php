7f5793e8fdb6ab4911502e0289a3e01f4a4661c2
Your Name <tecnicopaulo@outlook.pt>
2022-01-29 18:32:15 +0100

    edw

--- app/Models/Senra.php
+++ app/Models/Senra.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Senra extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+
+    protected $table = 'senra';
+    public $fillable = ['initials', 'title', 'description'];
+     
+    
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
 }
